c947ce262c60e74abc3cf5946eed8a3fb3ade66e
SandraMorales09 <114303948+SandraMorales09@users.noreply.github.com>
2025-03-27 14:54:56 -0600

    Delete practicas javascript directory

--- practicas javascript/Generar una tabla de multiplicar.js
+++
@@ -1,9 +0,0 @@
-function generarTablaMultiplicar(numero) {
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${numero} x ${i} = ${numero * i}`);
-    }
-}
-
-// Ejemplo de uso
-generarTablaMultiplicar(5);
-// Resultado: Genera la tabla del 5
--- practicas javascript/Revertir una cadena de texto.js
+++
@@ -1,7 +0,0 @@
-function revertirTexto(texto) {
-    return texto.split("").reverse().join("");
-}
-
-// Ejemplo de uso
-const textoOriginal = "Hola Mundo";
-console.log(revertirTexto(textoOriginal)); // Resultado: "odnuM aloH"
