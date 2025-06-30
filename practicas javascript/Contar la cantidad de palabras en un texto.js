b4060850f96b2d6d587f3519b4304a8b27b90a6e
Python-practicas-3-- <114303948+SandraMorales09@users.noreply.github.com>
2025-06-29 22:53:29 -0600

    Delete practicas javascript directory

--- practicas javascript/Contar la cantidad de palabras en un texto.js
+++
@@ -1,8 +0,0 @@
-function contarPalabras(texto) {
-    const palabras = texto.trim().split(/\s+/);
-    return palabras.length;
-}
-
-// Ejemplo de uso
-const textoEjemplo = "Este es un ejemplo de texto.";
-console.log(contarPalabras(textoEjemplo)); // Resultado: 6
